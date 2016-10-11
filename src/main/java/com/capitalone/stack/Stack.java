ea030614e476e112cc0be8aa1b423813cae2d05e
Howard Deiner <howard.deiner@deinersoft.com>
2016-10-11 17:14:25 -0500

    Implement everything except push throwing stack overflow and peek function.

--- src/main/java/com/capitalone/stack/Stack.java
+++ src/main/java/com/capitalone/stack/Stack.java
@@ -1,4 +1,24 @@
 package com.capitalone.stack;
 
 public class Stack {
+    String[] stack;
+    int stackIndex;
+
+    public Stack(int stackSize) {
+        stack = new String[stackSize];
+        stackIndex = 0;
+    }
+
+    public boolean isEmpty() {
+        return (stackIndex == 0);
+    }
+
+    public void push(String newElement) throws Exception {
+        stack[stackIndex++] = newElement;
+    }
+
+    public String pop() throws Exception {
+        if (isEmpty()) throw new Exception("stack underflow");
+        return stack[--stackIndex];
+    }
 }
--- src/test/java/com/capitalone/stack/StackTest.java
+++ src/test/java/com/capitalone/stack/StackTest.java
@@ -1,4 +1,60 @@
 package com.capitalone.stack;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class StackTest {
+
+    @Test
+    public void allocatedStackShouldBeEmpty() {
+        Stack myStack = new Stack(10);
+        assertThat(myStack.isEmpty(), is(true));
+    }
+
+    @Rule
+    public ExpectedException stackException = ExpectedException.none();
+
+    @Test
+    public void newStackPopThrowsException() throws Exception {
+        stackException.expect(Exception.class);
+        stackException.expectMessage("stack underflow");
+        new Stack(1).pop();
+    }
+
+    @Test
+    public void pushDogStackShouldNotBeEmpty() throws Exception {
+        Stack myStack = new Stack(10);
+        myStack.push("Dog");
+        assertThat(myStack.isEmpty(), is(false));
+    }
+
+    @Test
+    public void pushDogShouldPopDog() throws Exception {
+        Stack myStack = new Stack(10);
+        myStack.push("Dog");
+        assertThat(myStack.pop(), is("Dog"));
+    }
+
+    @Test
+    public void pushDogCatShouldPopCat() throws Exception {
+        Stack myStack = new Stack(10);
+        myStack.push("Dog");
+        myStack.push("Cat");
+        assertThat(myStack.pop(), is("Cat"));
+    }
+
+    @Test
+    public void pushDogCatMouseShouldPopPopCat() throws Exception {
+        Stack myStack = new Stack(3);
+        myStack.push("Dog");
+        myStack.push("Cat");
+        myStack.push("Mouse");
+        myStack.pop();
+        assertThat(myStack.pop(), is("Cat"));
+    }
+
 }
